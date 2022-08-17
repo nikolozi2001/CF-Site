44c848cc57a30f642d1639eda223303a256e750a
ნიკა ქაჩიბაია <nqachibaia@geostat.gov.ge>
2022-08-17 10:11:39 +0400

    for geostat

--- contact-view.php
+++ contact-view.php
@@ -16,7 +16,8 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" href="#" type="image/png">
+    <!-- <link rel="shortcut icon" href="#" type="image/png"> -->
+    <link rel="icon" type="image/png" href="https://www.geostat.ge/img/favicon.ico" />
     <title>Forms</title>
 </head>
 
@@ -42,7 +43,7 @@
         </div>
     </header>
     <div class="map container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.1839160758814!2d-73.8272838850948!3d40.735978444086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c260618039000d%3A0x4e4216fa03a650cf!2sMain%20St%2C%20Queens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sge!4v1608990375227!5m2!1sen!2sge" width="100%" height="385" frameborder="0" style="padding: 50px 0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1770.4713783809566!2d44.799650591727236!3d41.72988156638339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x404472b62853ba09%3A0xa05d0cb0a0c61544!2z4YOh4YOQ4YOl4YOQ4YOg4YOX4YOV4YOU4YOa4YOd4YOhIOGDoeGDouGDkOGDouGDmOGDoeGDouGDmOGDmeGDmOGDoSDhg5Thg6Dhg53hg5Xhg5zhg6Phg5rhg5gg4YOh4YOQ4YOb4YOh4YOQ4YOu4YOj4YOg4YOY!5e0!3m2!1ska!2sge!4v1660713108822!5m2!1ska!2sge" width="100%" height="385" frameborder="0" style="padding: 50px 0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
     <div class="contact container">
         <a href="#" class="link-contact">CONTACT</a>
@@ -51,32 +52,34 @@
         </h3>
     </div>
     <div class="form-container input-flex container">
-        <form name="frmContact" id="" frmContact"" method="post" action="" enctype="multipart/form-data" onsubmit="return validateContactForm()">
+        <form name="frmContact" id="frmcontact" frmContact"" method="post" action="" enctype="multipart/form-data" onsubmit="return validateContactForm()">
 
-            <div class="input-row">
-                <label for="name"></label> <span id="userName-info" class="info"></span><br /> <input type="text" class="form-input" placeholder="Name" name="userName" id="userName" required/>
-            </div>
-            <div class="input-row">
-                <label for="email"></label> <span id="userEmail-info" class="info"></span><br /> <input type="text" class="form-input" placeholder="Email" name="userEmail" id="userEmail" required/>
-            </div>
-            <div class="input-row">
-                <label for="subject"></label> <span id="subject-info" class="info"></span><br /> <input type="text" class="form-input" placeholder="Subject" name="subject" id="subject" required/>
-            </div>
-            <div class="input-row">
-                <label for="message"></label> <span id="userMessage-info" class="info"></span><br />
-                <textarea name="content" id="content" class="description" placeholder="Text" cols="60" rows="6" required></textarea>
-            </div>
-            <div>
-                <input type="submit" name="send" class="send-button" value="Send" />
+            <div class="inputs">
+                <div class="input-row">
+                    <label for="name"></label> <span id="userName-info" class="info"></span><br /> <input type="text" class="form-input" placeholder="Name" name="userName" id="userName" required />
+                </div>
+                <div class="input-row">
+                    <label for="email"></label> <span id="userEmail-info" class="info"></span><br /> <input type="text" class="form-input" placeholder="Email" name="userEmail" id="userEmail" required />
+                </div>
+                <div class="input-row">
+                    <label for="subject"></label> <span id="subject-info" class="info"></span><br /> <input type="text" class="form-input" placeholder="Subject" name="subject" id="subject" required />
+                </div>
+                <div class="input-row">
+                    <label for="message"></label> <span id="userMessage-info" class="info"></span><br />
+                    <textarea name="content" id="content" class="description" placeholder="Text" cols="60" rows="6" required></textarea>
+                </div>
+                <div>
+                    <input type="submit" name="send" class="send-button" value="Send" />
 
-                <div id="statusMessage">
-                    <?php
-                    if (!empty($message)) {
-                    ?>
-                        <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
-                    <?php
-                    }
-                    ?>
+                    <div id="statusMessage">
+                        <?php
+                        if (!empty($message)) {
+                        ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </form>
@@ -90,8 +93,8 @@
             </span>
             <ul class="contact-ul">
                 <li class="contact-li bold">Email</li>
-                <li class="contact-li">Example@yahoo.com</li>
-                <li class="contact-li">Examplemail@yahoo.com</li>
+                <li class="contact-li">info@geostat.ge</li>
+                <li class="contact-li">info@geostat.ge</li>
             </ul>
         </div>
         <div class="contact-flex">
@@ -100,8 +103,8 @@
             </span>
             <ul class="phone-ul">
                 <li class="phone-li bold">Phone</li>
-                <li class="phone-li">+0 320 422 4254</li>
-                <li class="phone-li">+0 342 552 523</li>
+                <li class="phone-li">(+995 32) 236 72 10</li>
+                <li class="phone-li">(+995 32) 260 11 60</li>
             </ul>
         </div>
         <div class="contact-flex">
@@ -110,8 +113,8 @@
             </span>
             <ul class="location-ul">
                 <li class="location-li bold">Location</li>
-                <li class="location-li">Main Str Chicago Ilinois</li>
-                <li class="location-li">Main Street New York City</li>
+                <li class="location-li">ცენტრალური ოფისი: ქ. თბილისი</li>
+                <li class="location-li">0180, ცოტნე დადიანის ქ. 30</li>
             </ul>
         </div>
     </div>
@@ -124,6 +127,6 @@
     }
 </script>
 <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
-<script src="script.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
 
 </html>
